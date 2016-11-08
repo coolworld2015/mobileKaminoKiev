755d7ae1dff569a237dd54be41f0c091c3631a26
coolworld2015 <wintermute2015@ukr.net>
2016-11-08 16:28:18 +0200

    [general] - refactoring

--- app/src/components/collection/collectionDetails.js
+++ app/src/components/collection/collectionDetails.js
@@ -75,7 +75,7 @@ class CollectionDetails extends Component {
 
                     {pic}
 
-                    <Text style={styles.welcome}>
+                    <Text style={styles.welcome1}>
                         {this.state.pushEvent.name}
                     </Text>
 
@@ -109,6 +109,12 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
+    },
+    welcome1: {
+        fontSize: 18,
+        textAlign: 'center',
+        margin: 10,
+        fontWeight: 'bold'
     },
     welcome: {
         fontSize: 18,
